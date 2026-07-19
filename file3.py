98c601e33961b8cd33a2a00b865a6f3b27047fe0
AliFoot <mouadmdp@gmail.com>
2026-07-19 02:21:26 +0100

    Create file3.py

---
+++ file3.py
@@ -0,0 +1,13 @@
+# 1. Defining the function
+def greet_friends(person1, person2, person3):
+    print(f"Hello {person1}!")
+    print(f"Hey there {person2}!")
+    print(f"Hi {person3}!")
+
+
+# 2. Assigning multiple variables on one line
+x, y, z = "Jack", "Alice", "Bob"
+
+
+# 3. Calling the function using our variables
+greet_friends(x, y, z)
